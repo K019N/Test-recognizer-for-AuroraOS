d00e1a4cd491edf138a5cd08d3d1caaf76768d59
K019N <63264464+K019N@users.noreply.github.com>
2024-05-25 13:31:10 +0300

    test server connection

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,13 @@
 import cv2 as cv
 import numpy as np
 
-from .camera_reciver import *
+from camera_reciver import *
 
 #TODO: buttons not working now
 #def back_button(*args):
 #    pass
 
 camera = cv.VideoCapture(0) 
-app = CameraReciver()
+app = CameraReciver(id="1")
 if (app.exists_device(camera)): app.runCamera(camera)
 #cv.createButton("Back", back_button, None, cv.QT_PUSH_BUTTON,1)
